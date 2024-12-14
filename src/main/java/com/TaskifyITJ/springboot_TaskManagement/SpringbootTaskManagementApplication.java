abc4efc2d8b32885a5e1977b229cf8f7c6ae8366
Juan Manuel Chavez Peralta <127172653+Juanblink@users.noreply.github.com>
2024-12-14 11:36:36 -0800

    Initial commit with project structure

---
+++ src/main/java/com/TaskifyITJ/springboot_TaskManagement/SpringbootTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.TaskifyITJ.springboot_TaskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskManagementApplication.class, args);
+	}
+
+}
