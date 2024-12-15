351c4c85ae138691f0a9ea58f279ea67dbfdd2cc
Juan Manuel Chavez Peralta <127172653+Juanblink@users.noreply.github.com>
2024-12-14 19:54:34 -0800

    Milestone 1: Project Setup and Git Basics

---
+++ src/main/java/com/TaskifyITJ/springboot_TaskManagement/HealthController.java
@@ -0,0 +1,25 @@
+package com.TaskifyITJ.springboot_TaskManagement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.time.LocalDateTime;
+
+@RestController
+public class HealthController {
+
+    // Endpoint 1 TEST
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+
+    // Endpoint 2
+    @GetMapping("/health/details")
+    public Map<String, String> healthDetails() {
+        return Map.of(
+                "status", "UP",
+                "timestamp", LocalDateTime.now().toString()
+        );
+    }
+}
